77df1ce6133eb39d16cfe46f4ccc00d185193433
xkyn0912 <2087643906@qq.com>
2022-07-16 10:55:48 +0800

    第一次提交

--- src/main/java/com/example/test/work01.java
+++ src/main/java/com/example/test/work01.java
@@ -2,6 +2,7 @@ package com.example.test;
 
 public class work01 {
     public static void main(String[] args) {
-        System.out.println("你好");
+        System.out.println("你好:");
+        System.out.println("mxk");
     }
 }
