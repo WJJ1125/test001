0483907cc6a63d5008cbc62b351df9f28881c254
WJJ1125 <3154189600@qq.com>
2022-07-16 10:54:37 +0800

    第二次

--- src/main/java/com/example/test/work01.java
+++ src/main/java/com/example/test/work01.java
@@ -2,6 +2,8 @@ package com.example.test;
 
 public class work01 {
     public static void main(String[] args) {
-        System.out.println("你好");
+        System.out.println("word");
+        System.out.println("你好啊");
+        System.out.println("xxx");
     }
 }
